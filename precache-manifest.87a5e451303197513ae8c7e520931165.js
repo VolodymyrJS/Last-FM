1343f9f3a6f290a46538a6797410ec05fd5f5c9e
root <root@GFL-1951.gfl.geeksforless.net>
2019-07-25 18:45:54 +0300

    Updates

---
+++ precache-manifest.87a5e451303197513ae8c7e520931165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa69c14952a1f11bb7bb404e1b095288",
+    "url": "/Last-FM/index.html"
+  },
+  {
+    "revision": "f508fd9d5988da8aed6c",
+    "url": "/Last-FM/static/css/main.80a8dc42.chunk.css"
+  },
+  {
+    "revision": "bffbe503a242a8381068",
+    "url": "/Last-FM/static/js/2.f13c5dc7.chunk.js"
+  },
+  {
+    "revision": "f508fd9d5988da8aed6c",
+    "url": "/Last-FM/static/js/main.c4e7fd74.chunk.js"
+  },
+  {
+    "revision": "87f5279372c7ec83f2fb",
+    "url": "/Last-FM/static/js/runtime~main.54553cfb.js"
+  }
+]);
