bb81f9bd74ef510837ccd544b3f58185991bff5e
root <root@GFL-1951.gfl.geeksforless.net>
2019-07-25 18:58:51 +0300

    Replace http:// for https://

--- src/fetchConfig.js
+++ src/fetchConfig.js
@@ -1,8 +1,8 @@
-const url = 'http://ws.audioscrobbler.com/2.0/';
+const url = 'https://ws.audioscrobbler.com/2.0/';
 const method = '?method=chart.gettoptracks&api_';
 const key = 'key=338b9e08c1e5acbef9302a70d6ea86f1';
 const format = '&format=json';
-const url_about = 'http://ws.audioscrobbler.com/2.0/';
+const url_about = 'https://ws.audioscrobbler.com/2.0/';
 const key_about = 'key=338b9e08c1e5acbef9302a70d6ea86f1';
 const format_about = '&format=json';
 
